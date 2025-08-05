6d9c2b8e9e67ef82a670bc96a63ba2c13af4c496
benzayedabderrahim <benzayedabderrahim@gmail.com>
2025-08-06 00:16:14 +0100

    new

--- videos/fetch_youtube.py
+++ videos/fetch_youtube.py
@@ -208,7 +208,7 @@ def fetch_and_save_videos(query, max_videos=20):
             for item in search_data.get("items", []) 
             if item.get("id", {}).get("videoId")
         ]
-        # fetching data
+        # fetching data from url
         with ThreadPoolExecutor(max_workers=5) as executor:
             for video_id in video_ids:
                 executor.submit(process_video, video_id, query)
